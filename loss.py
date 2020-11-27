95dc773961648ca67adc0e3da7d5e17f1176c5b8
KimJaechang <jaechang@postech.ac.kr>
2020-11-27 07:01:21 +0000

    Fix spatial entropy

--- loss.py
+++ loss.py
@@ -35,22 +35,25 @@ def compute_L1_entropy(n_fft) :
     return compute
 
 def compute_entropy(inputs, targets, n_fft):
+    eps = 0.001
 
     mono_inputs = torch.mean(inputs, dim=1)
     mono_targets = torch.mean(targets, dim=1)
-    spec_inputs = torch.stft(mono_inputs, n_fft=n_fft, centor=False, normalized=False, onesided=True)[:, :, :, 0]       # [B, N(freq), F(frame)]
-    spec_targets = torch.stft(mono_targets, n_fft=n_fft, centor=False, normalized=False, onesided=True)[:, :, :, 0]
+    spec_inputs = torch.stft(mono_inputs, n_fft=n_fft, center=False, normalized=False, onesided=True)[:, :, :, 0]       # [B, N(freq), F(frame)]
+    spec_targets = torch.stft(mono_targets, n_fft=n_fft, center=False, normalized=False, onesided=True)[:, :, :, 0]
 
     repeat_inputs = torch.unsqueeze(spec_inputs.permute(0, 2, 1), 2)    # [B, F, 1, N]
     repeat_inputs = repeat_inputs.repeat(1, 1, repeat_inputs.shape[1], 1) # [B, F, F, N]
-    repeat_inputs = repeat_inputs.view(repeat_inputs.shape[0], -1, repeat_inputs.shape[2])
+    repeat_inputs = repeat_inputs.view(repeat_inputs.shape[0], -1, repeat_inputs.shape[3])
 
     repeat_targets = spec_targets.permute(0, 2, 1)    # [B, F, N]
     repeat_targets = repeat_targets.repeat(1, repeat_targets.shape[1], 1) # [B, F*F, N]
 
-    distance = torch.mean((repeat_inputs - repeat_targets) ** 2, dim=2).view(spec_inputs.shape[0], spec_inputs.shape[2], spec_inputs.shape[2])
+    #distance = torch.mean((repeat_inputs - repeat_targets) ** 2, dim=2).view(spec_inputs.shape[0], spec_inputs.shape[2], spec_inputs.shape[2])
+    distance = ((spec_inputs - spec_targets) ** 2)
+    distance = distance + torch.ones_like(distance) * eps
     entropy = torch.log(torch.pow(distance, -1))
-    entropy_sum = torch.mean(torch.mean(entropy, dim=2), dim=1)
+    entropy_sum = torch.mean(torch.mean(entropy, dim=2), dim=1) * -1
 
     return entropy_sum
 
